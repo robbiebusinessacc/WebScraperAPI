2f5086f1fb1910e0c8695ae90725a87cfb822c9a
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-23 14:07:22 -0500

    Update scraper.py

--- template_web_scraper/scraper.py
+++ template_web_scraper/scraper.py
@@ -6,9 +6,9 @@ class Scraper:
 
     def get_html(self):
         try:
-            response = requests.get(self.url)
+            response = requests.get(f'http://localhost:5000/scrape?url={self.url}')
             response.raise_for_status()
-            return response.text
+            return response.json()
         except requests.exceptions.RequestException as e:
             print(e)
             return None
