8cb328dce8953100ffd7273e18dcd7d1c38a1e34
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-23 14:11:21 -0500

    Update parser.py

--- template_web_scraper/parser.py
+++ template_web_scraper/parser.py
@@ -1,15 +1,17 @@
 from bs4 import BeautifulSoup
 
 class Parser:
-    def __init__(self, html):
-        self.soup = BeautifulSoup(html, 'html.parser')
+    def __init__(self, data):
+        self.data = data
+        self.soup = BeautifulSoup(self.data, 'html.parser')
 
-    def get_data(self):
-        data = []
-        table = self.soup.find("table", {"id": "main_table_countries_today"})
-        rows = table.find_all("tr")
-        for row in rows:
-            cols = row.find_all("td")
-            cols = [ele.text.strip() for ele in cols]
-            data.append([ele for ele in cols if ele])
-        return data
+    def parse_data(self):
+        parsed_data = []
+        tables = self.soup.find_all("table")
+        for table in tables:
+            rows = table.find_all("tr")
+            for row in rows:
+                cols = row.find_all("td")
+                cols = [ele.text.strip() for ele in cols]
+                parsed_data.append([ele for ele in cols if ele])
+        return parsed_data
