f37ecfd4182c651b82ae1e7a11f99f2af241aad6
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-24 10:33:27 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
@@ -8,7 +9,7 @@ setup(
     description="A web scraping API that allows users to easily extract data from any website by simply providing the URL. The API utilizes advanced parsing and data storage techniques to ensure accurate and efficient data extraction. The package is easy to install and use, making it perfect for data scientists, researchers, and developers looking to quickly and easily access web data.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/robbiebusinessacc/web_scraper_api/",
+    url="https://github.com/robbiebusinessacc/WebScraperAPI/",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -19,6 +20,6 @@ setup(
     install_requires=[
         "beautifulsoup4",
         "requests",
-        "selenium", 
+        "fake_useragent"
     ],
 )
