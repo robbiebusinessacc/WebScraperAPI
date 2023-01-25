36b1bc27a154bca70eb29d5d9303ff4c9a5a9549
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-25 10:13:39 -0500

    Update WebScraperAPI.py

--- WebScraperAPI.py
+++ WebScraperAPI.py
@@ -1,34 +1,24 @@
-# WebScraperAPI.py
-from flask import Flask, request
-from WebScraperAPI.scraper import Scraper
-from WebScraperAPI.parser import Parser
-from WebScraperAPI.data_storage import DataStorage
+class WebScraperAPI:
+    def __init__(self, file_name='example_output.csv'):
+        self.file_name = file_name
 
-app = Flask(__name__)
+    def scrape_website(self, url):
+        if not url:
+            raise ValueError("Error: No URL provided. Please specify a URL to scrape.")
 
-@app.route('/scrape', methods=['GET'])
-def scrape_website():
-    url = request.args.get('url')
-    if not url:
-        return "Error: No URL provided. Please specify a URL to scrape."
+        # Scrape website
+        scraper_obj = Scraper(url)
+        data = scraper_obj.get_html()
 
-    # Scrape website
-    scraper_obj = Scraper(url)
-    data = scraper_obj.get_html()
-
-    if data:
-        # Parse the data
-        parser_obj = Parser(data)
-        parsed_data = parser_obj.parse_data()
-        
-        # Save the data
-        file_name = 'example_output.csv'
-        data_storage_obj = DataStorage(parsed_data, file_name)
-        data_storage_obj.save_data()
-        
-        return "Scraped data from {}".format(url)
-    else:
-        return "An error occurred while scraping the website"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        if data:
+            # Parse the data
+            parser_obj = Parser(data)
+            parsed_data = parser_obj.parse_data()
+            
+            # Save the data
+            data_storage_obj = DataStorage(parsed_data, self.file_name)
+            data_storage_obj.save_data()
+            
+            return "Scraped data from {} and saved in file: {}".format(url, self.file_name)
+        else:
+            return "An error occurred while scraping the website"
