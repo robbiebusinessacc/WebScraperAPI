c96b286585004265ef714086267aded6b05053ed
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-01-25 08:58:52 -0500

    Update scraper.py

--- WebScraperAPI/scraper.py
+++ WebScraperAPI/scraper.py
@@ -3,12 +3,10 @@ import requests
 class Scraper:
     def __init__(self, url):
         self.url = url
-        self.proxy = 'http://proxy_address:proxy_port'
-        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
 
     def get_html(self):
         try:
-            response = requests.get(self.url, proxies={'http': self.proxy, 'https': self.proxy}, headers=self.headers)
+            response = requests.get(self.url)
             response.raise_for_status()
             return response.text
         except requests.exceptions.RequestException as e:
